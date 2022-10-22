68a31ae809365f163d64c12bca405677e1264109
AnniesAmare <annethearendtorff@gmail.com>
2022-10-22 12:58:00 +0200

    Added util class and did program cleanup

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
 
 var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
 server.Start();
@@ -12,74 +7,69 @@ server.Start();
 Console.WriteLine("Server is started...");
 
 //DATA MODEL
-List<Category> category = new List<Category>();
-category.Add(new Category(1, "Beverages"));
-category.Add(new Category(2, "Condiments"));
-category.Add(new Category(3, "Confections"));
+List<Category> defaultCategories = new List<Category>
+{
+    { new(1, "Beverages") },
+    {new (2, "Condiments")},
+    {new (3, "Confections")},
 
-var DataModel = new DataModel(category);
+};
+var DataModel = new DataModel(defaultCategories);
 
 while (true)
 {
-    // get client connection
+    // get new client connection
     var newClient = server.AcceptTcpClient();
+
     // once connected, create new thread to handle communication
     Thread thread = new Thread(HandleClient);
     thread.Start(newClient);
 
     // the handle method used on all connected clients
-    void HandleClient(object obj)
+    void HandleClient(object clientObj)
     {
-        TcpClient client = (TcpClient)obj;
+        TcpClient client = (TcpClient)clientObj;
         Console.WriteLine("Connected to new client");
 
-        var stream = client.GetStream();
-        var buffer = new byte[1024];
         var response = new Response();
 
         try
         {
-            var readCount = stream.Read(buffer, 0, buffer.Length);
-
-            var requestData = System.Text.Encoding.UTF8.GetString(buffer, 0, readCount);
-            Console.WriteLine("Received request: {0}", requestData);
-            var requestFromJson = JsonSerializer.Deserialize<Request>(requestData);
-
-            Console.WriteLine(requestFromJson.Method);
+            var request = client.ReadRequest();
 
             //REQUEST HANDLING
-            response = requestFromJson.checkForBadRequest();
+            response = request.checkForBadRequest();
             if (!response.Status.Contains('4')) //Making sure the request is not a bad request
             {
                 Console.WriteLine("Request approved");
-                var method = requestFromJson.Method;
+                var method = request.Method;
 
                 // HANDLING ECHO METHOD
                 if (method == "echo")
                 {
                     response.Status = "1 Ok";
-                    response.Body = requestFromJson.Body;
+                    response.Body = request.Body;
                 }
 
                 // HANDLING READ METHOD
                 if (method == "read")
                 {
-                    var requestedCid = requestFromJson.getPathCid();
+                    var requestedCid = request.getPathCid();
                     response = DataModel.read(requestedCid);
                 }
 
                 //HANDLING UPDATE METHOD
                 if (method == "update")
                 {
-                    var requestedCid = requestFromJson.getPathCid();
-                    var updatedCategoryFromJson = JsonSerializer.Deserialize<Category>(requestFromJson.Body);
+                    var requestedCid = request.getPathCid();
+                    var updatedCategoryFromJson = request.Body.FromJson<Category>();
                     response = DataModel.update(requestedCid, updatedCategoryFromJson);
                 }
 
                 //HANDLING CREATE METHOD
                 if (method == "create")
                 {
-                    var createdCategoryFromJson = JsonSerializer.Deserialize<Category>(requestFromJson.Body);
+                    var createdCategoryFromJson = request.Body.FromJson<Category>();
                     var createdCategoryName = createdCategoryFromJson.Name;
                     response = DataModel.create(createdCategoryName);
                 }
@@ -87,26 +77,21 @@ while (true)
                 //HANDLING DELETE METHOD
                 if (method == "delete")
                 {
-                    var requestedCid = requestFromJson.getPathCid();
+                    var requestedCid = request.getPathCid();
                     response = DataModel.delete(requestedCid);
                 }
 
             }
             //SEND JSON RESPONSE 
-            var responseToJson = JsonSerializer.Serialize<Response>(response);
+            var responseToJson = response.ToJson();
             Console.WriteLine("Created response: {0}", responseToJson);
-
-            var responseBuffer = Encoding.UTF8.GetBytes(responseToJson);
-            stream.Write(responseBuffer);
-
+            client.SendResponse(responseToJson);
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            Console.WriteLine("No request recieved"); //not needed
+            //Console.WriteLine(e);
+            Console.WriteLine("Connection closed. No request received");
         }
-
-        stream.Close();
     }
 
 
---
+++ Assignment3/Util.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+public static class Util
+{
+    public static string ToJson(this object data)
+    {
+        return JsonSerializer.Serialize(data);
+    }
+
+    public static T? FromJson<T>(this string element) => JsonSerializer.Deserialize<T>(element);
+    
+    public static void SendResponse(this TcpClient client, string response)
+    {
+        var strm = client.GetStream();
+        var msg = Encoding.UTF8.GetBytes(response);
+        strm.Write(msg, 0, msg.Length);
+        strm.Close();
+    }
+
+    public static Request? ReadRequest(this TcpClient client)
+    {
+        var strm = client.GetStream();
+        //strm.ReadTimeout = 250;
+        byte[] reqst = new byte[2048];
+        using (var memStream = new MemoryStream())
+        {
+            int bytesread = 0;
+            do
+            {
+                bytesread = strm.Read(reqst, 0, reqst.Length);
+                memStream.Write(reqst, 0, bytesread);
+
+            } while (bytesread == 2048);
+
+            var requestData = Encoding.UTF8.GetString(memStream.ToArray());
+            Console.WriteLine("Received request: {0}", requestData); //used for testing
+            return JsonSerializer.Deserialize<Request>(requestData);
+        }
+    }
+
+
+
+}
